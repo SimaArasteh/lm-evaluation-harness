437c5af9838c324e3bec55535c023b2d9dcb6c93
Sima Arasteh <arasteh@b11-09.hpc.usc.edu>
2025-06-14 23:13:38 -0700

    adding the task of harnesserrors fixing

--- lm_eval/tasks/injectvul/harnesserrors_ds.py
+++ lm_eval/tasks/injectvul/harnesserrors_ds.py
@@ -2,14 +2,15 @@ import datasets
 import glob
 import os
 
-class FuzzGenDataset(datasets.GeneratorBasedBuilder):
+class FixHarnessError(datasets.GeneratorBasedBuilder):
     def _info(self):
         return datasets.DatasetInfo(
-            description="Dataset of fake main bodies with instructions to generate libfuzzer harness.",
+            description="Dataset of harnesses with build errors",
             features=datasets.Features(
                 {
                     "id": datasets.Value("string"),
                     "function_body": datasets.Value("string"),
+                    "builderror": datasets.Value("string"),
                     "instruction": datasets.Value("string"),
                     "expected_output": datasets.Value("string"),
                 }
@@ -19,7 +20,7 @@ class FuzzGenDataset(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
-        dataset_path = "/home1/arasteh/BinVulGen/ai_agent/llm_response/"  # Folder with .txt files
+        dataset_path = "/home1/arasteh/BinVulGen/ai_agent/fixed_error/harness_errors/"  # Folder with .txt files
         return [
             datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": dataset_path}),
             datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": dataset_path}),
@@ -28,7 +29,11 @@ class FuzzGenDataset(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath):
         instruction = (
-            "write a libfuzzer style fuzzing harness for this function"
+            "You are a security-focused code assistant helping with fuzzing infrastructure."
+
+            "I have generated the following fuzzing harness for a C/C++ function. However, when I attempt to build the harness using the OSS-Fuzz build system (with clang and AddressSanitizer), I encounter these build errors."
+
+            "Please analyze the harness and the build errors. rewrite the fuzzing harness and try to fix the build errors."
         )
 
         file_list = glob.glob(os.path.join(filepath, "*.txt"))
@@ -36,12 +41,17 @@ class FuzzGenDataset(datasets.GeneratorBasedBuilder):
         for i, file_path in enumerate(file_list):
             try:
                 with open(file_path, "r") as f:
-                    function_body = f.read().strip()
+                    content = f.read().strip()
+                
                 filename = os.path.basename(file_path)
                 function_name = filename.split(".")[0]
+                content_parts = content.split("++++++++++")
+                function_body = content_parts[0]
+                builderror = content_parts[1]
                 yield i, {
                     "id": function_name,
                     "function_body": function_body,
+                    "builderror": builderror,
                     "instruction": instruction,
                     "expected_output": "",  # Leave blank; model will generate
                 }
