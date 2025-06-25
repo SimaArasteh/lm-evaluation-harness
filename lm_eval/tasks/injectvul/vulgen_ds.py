102479924200441ee597bfc4e0520f45a9026b0c
Sima Arasteh <arasteh@a03-06.hpc.usc.edu>
2025-06-25 15:52:54 -0700

    improving the prompt

--- lm_eval/tasks/injectvul/vulgen_ds.py
+++ lm_eval/tasks/injectvul/vulgen_ds.py
@@ -1,6 +1,7 @@
 import datasets
 import glob
 import os
+import random
 
 class InjectUAFDataset(datasets.GeneratorBasedBuilder):
     def _info(self):
@@ -27,13 +28,19 @@ class InjectUAFDataset(datasets.GeneratorBasedBuilder):
         ]
 
     def _generate_examples(self, filepath):
-        instruction = (
-            "Inject a use-after-free vulnerability into the following function. "
-            "Use only variables and pointers inside the function. Do not generate a new variable."
-            "You are only allowed to modify the function body. Do not remove or rename the function. "
+        '''instruction = (
+           "You are given a C/C++ function. Inject a use-after-free vulnerability in to the following function by using one of the techniques below."
+           "Technique1: apply these steps in order. 1. Choose one of the existing pointers in the function. Do not create a new variable."
+           "2. use function free to free the allocated memory. 3. reuse that pointer after function free. try to put this line as far as possible to function free"
+           "Technique2: 1.Identify an existing pointer in the function. 2. Create a pointer alias by assigning the existing pointer to another pointer variable within the function"
+           "3.Free the original pointer to deallocate the memory it points to using free function. 4.After the memory is freed, access it again using the alias pointer. This access can be a read, write."
+           "You are only allowed to modify the function body. Do not remove or rename the function. "
             "Preserve the original structure as much as possible."
-        )
-
+           )'''
+        import joblib
+        list_prompts = joblib.load("/home1/arasteh/BinVulGen/ai_agent/use-after-free-listprompts.pkl")
+        selected_prompt = random.choice(list_prompts)
+        instruction = selected_prompt
         file_list = glob.glob(os.path.join(filepath, "*.txt"))
 
         for i, file_path in enumerate(file_list):
