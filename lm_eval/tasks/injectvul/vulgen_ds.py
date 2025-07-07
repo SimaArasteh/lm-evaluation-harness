26181cb9e937f50588d0f0e58dec65127a20677f
Sima Arasteh <arasteh@discovery1.hpc.usc.edu>
2025-07-07 15:30:15 -0700

    accepting prompts from patterns

--- lm_eval/tasks/injectvul/vulgen_ds.py
+++ lm_eval/tasks/injectvul/vulgen_ds.py
@@ -28,19 +28,15 @@ class InjectUAFDataset(datasets.GeneratorBasedBuilder):
         ]
 
     def _generate_examples(self, filepath):
-        '''instruction = (
-           "You are given a C/C++ function. Inject a use-after-free vulnerability in to the following function by using one of the techniques below."
-           "Technique1: apply these steps in order. 1. Choose one of the existing pointers in the function. Do not create a new variable."
-           "2. use function free to free the allocated memory. 3. reuse that pointer after function free. try to put this line as far as possible to function free"
-           "Technique2: 1.Identify an existing pointer in the function. 2. Create a pointer alias by assigning the existing pointer to another pointer variable within the function"
-           "3.Free the original pointer to deallocate the memory it points to using free function. 4.After the memory is freed, access it again using the alias pointer. This access can be a read, write."
-           "You are only allowed to modify the function body. Do not remove or rename the function. "
-            "Preserve the original structure as much as possible."
-           )'''
-        import joblib
-        list_prompts = joblib.load("/home1/arasteh/BinVulGen/ai_agent/use-after-free-listprompts.pkl")
-        selected_prompt = random.choice(list_prompts)
-        instruction = selected_prompt
+       
+        pattern_paths = "/home1/arasteh/BinVulGen/llm_json_patterns/"
+        list_patterns = glob.glob(os.path.join(pattern_paths, "*.txt"))
+        selected_pattern = random.choice(list_patterns)
+        with open(selected_pattern, "r") as pat:
+            pattern_content = pat.read()
+
+        instruction = "I will give you a pattern of use-after-free. use this pattern to inject vulnerability in the following function. Pattern:"+pattern_content
+        
         file_list = glob.glob(os.path.join(filepath, "*.txt"))
 
         for i, file_path in enumerate(file_list):
