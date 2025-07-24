1132488a1e79a794314254e49616ad76fdb9df7d
Sima Arasteh <arasteh@b11-09.hpc.usc.edu>
2025-07-24 14:01:33 -0700

    update vulgen task for bug injection

--- lm_eval/tasks/injectvul/vulgen_ds.py
+++ lm_eval/tasks/injectvul/vulgen_ds.py
@@ -2,6 +2,7 @@ import datasets
 import glob
 import os
 import random
+import joblib
 
 class InjectUAFDataset(datasets.GeneratorBasedBuilder):
     def _info(self):
@@ -31,12 +32,15 @@ class InjectUAFDataset(datasets.GeneratorBasedBuilder):
        
         pattern_paths = "/home1/arasteh/BinVulGen/llm_json_patterns/"
         list_patterns = glob.glob(os.path.join(pattern_paths, "*.txt"))
-        selected_pattern = random.choice(list_patterns)
-        with open(selected_pattern, "r") as pat:
-            pattern_content = pat.read()
-
-        instruction = "I will give you a pattern of use-after-free. use this pattern to inject vulnerability in the following function. if any added lines require a new header file just add that header file. Otherwise do not add any other functions. here is the Pattern:"+pattern_content
         
+
+        instruction = "I will give you a pattern of use-after-free in real CVEs and then a C/C++ function body from mupdf project. I want you to carefully follow the orders in the pattern and mimic it to inject use-after-free vulnerability into the function body. Remember not to use any function names in the pattern. for memory allocation or deallocation only use functions in mupdf."
+        
+        '''vul = "/home1/arasteh/BinVulGen/pattern_functions/cves/comps_mrtree_unite.txt"
+        with open(vul, "r") as v:
+            vul_content = v.read()
+
+        instruction = "I will give you an exmaple of use-after-free vulnerability including the patch and the vulnerable function. Then I will give you another function from mupdf project. Inspiring from the example, inject use-after-free vulnerability into this function. To use any function (e.g for free ) remember to use functions of mupdf. Vulnerability Example:  "+vul_content'''
         file_list = glob.glob(os.path.join(filepath, "*.txt"))
 
         for i, file_path in enumerate(file_list):
@@ -45,6 +49,12 @@ class InjectUAFDataset(datasets.GeneratorBasedBuilder):
                     function_body = f.read().strip()
                 filename = os.path.basename(file_path)
                 function_name = filename.split(".")[0]
+                selected_pattern = random.choice(list_patterns)
+                with open(selected_pattern, "r") as pat:
+                    pattern_content = pat.read()
+                pattern_name = selected_pattern.split("/")[-1].split(".")[0]
+                joblib.dump(pattern_content, "/home1/arasteh/BinVulGen/binvulgen_results/qwen/"+function_name+"/selected_pattern/"+pattern_name+".pkl")
+                instruction = instruction+ " here is the pattern"+pattern_content
                 module_name = filename.split(".")[1]
                 if module_name.lower() == ".c":
                     instruction = instruction+" Remember:This function is written in C language.just use C specific keywords and syntax"
